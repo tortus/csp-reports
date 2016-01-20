6c7fbf6140783ced27aa18d03765b7bb0a1ba72e
William Makley <wmakley@tortus.com>
2016-01-20 14:20:47 -0500

    don't send emails for duplicate violation reports

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'https://rubygems.org'
 gem 'sinatra'
 gem 'sinatra-contrib'
 gem 'pony'
+gem 'data_mapper'
+gem 'dm-sqlite-adapter'
 
 group :development do
   gem 'dotenv'
--- app.rb
+++ app.rb
@@ -2,11 +2,27 @@ require 'sinatra'
 require 'sinatra/multi_route'
 require 'json'
 require 'pony'
+require 'data_mapper'
+require 'digest'
+require 'digest/bubblebabble'
 
 unless settings.production?
   require 'dotenv'
   Dotenv.load
 end
+
+DataMapper.setup(:default, 'sqlite:csp-reports.sqlite')
+
+class Report
+  include DataMapper::Resource
+  property :sha256, Text, key: true
+  property :body, Text, required: true
+  property :count, Integer, default: 1, min: 1, required: true
+end
+
+DataMapper.finalize
+DataMapper.auto_migrate! if ENV['MIGRATE'] =~ /\At(true)?|y(es)?|1/i
+
 
 set :logging, true
 set :csp_report_recipients, ENV.fetch('CSP_REPORT_RECIPIENTS')
@@ -14,17 +30,33 @@ set :csp_report_sender, ENV.fetch('CSP_REPORT_SENDER')
 
 route :get, :post, '/' do
   request.body.rewind
-  json = JSON.parse(request.body.read)
+  raw_text = request.body.read
 
-  text = JSON.pretty_generate(json)
-  logger.info text
+  sha256 = Digest::SHA256.bubblebabble(raw_text)
+  report = Report.get(sha256)
+  if !report
+    json = JSON.parse(raw_text)
+    formatted_json = JSON.pretty_generate(json)
 
-  Pony.mail(
-    :to => settings.csp_report_recipients,
-    :from => settings.csp_report_sender,
-    :subject => 'CSP violation',
-    :body => text
-  )
+    report = Report.create!(
+      sha256: sha256,
+      body: raw_text,
+      count: 1
+    )
+
+    logger.info "Registered new problem:\n#{formatted_json}"
+
+    Pony.mail(
+      :to => settings.csp_report_recipients,
+      :from => settings.csp_report_sender,
+      :subject => 'CSP violation',
+      :body => formatted_json
+    )
+  else
+    report.count += 1
+    report.save!
+    logger.info "Registered new occurrence of problem #{sha256} Count: #{report.count}"
+  end
 
   status 204
   body nil
