61ed70467aa92e00c48e287a2cd50fe3350f9f9e
William Makley <wmakley@tortus.com>
2016-12-23 15:16:55 -0500

    add WIP admin views

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,3 +1,22 @@
 class AdminController < Sinatra::Base
+
   set :views, CSPReports.root + '/app/views/admin'
+
+  provides 'html'
+
+  get '/' do
+    @domains = Domain.all
+    erb :index
+  end
+
+  get '/domains/:domain' do
+    @reports = Report.where(domain: params[:domain]).all
+    erb :domain
+  end
+
+  get '/reports/:id' do
+    @report = Report.find(params[:id])
+    erb :report
+  end
+
 end
