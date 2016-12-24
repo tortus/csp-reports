003ba3eea55071d5fba840198bd0cddca895c2e4
William Makley <wmakley@tortus.com>
2016-12-24 13:06:06 -0500

    order admin index by count

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,7 +1,7 @@
 class Domain
 
   def self.all
-    sql = 'SELECT domain, SUM(count) AS count FROM reports GROUP BY domain'
+    sql = 'SELECT domain, SUM(count) AS count FROM reports GROUP BY domain ORDER BY count DESC'
     records = DataMapper.repository.adapter.select(sql)
   end
 
