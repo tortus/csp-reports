a1d6704fec27ec16c16426eecc76f3daba568f41
William Makley <wmakley@tortus.com>
2017-10-25 16:11:27 -0400

    redirect stdout to a file in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 # frozen_string_literal: true
 class ApplicationController < Sinatra::Base
-  if CSPReports.env == :development
-    enable :logging
-  else
-    set :logging, nil
-    use Rack::Logger
-  end
+  enable :logging
 end
